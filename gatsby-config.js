d27a2e7389787294f7a4d787533a425a8892023f
Richard Germaine <richard.germaine@Richards-MacBook-Pro.local>
2020-03-06 22:27:48 -0700

    l

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,13 +36,8 @@ module.exports = {
           urls: ["/fonts/fonts.css"]
         }
       }
-    },
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/`, `/404/`]
-      }
     }
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
