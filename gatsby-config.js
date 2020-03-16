c595d3eb294d747150788e2dfcf884fb87c04a64
Richard Germaine <richard.germaine@Richards-MacBook-Pro.local>
2020-03-16 13:37:16 -0600

    clean up

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,9 +30,5 @@ module.exports = {
     `gatsby-plugin-sass`,
 
     "gatsby-plugin-offline"
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ]
 };
